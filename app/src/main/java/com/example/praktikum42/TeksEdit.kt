8a9ebba9e12422a6ea92b4bed3d202a17464add0
annisadiann <annisadian0412@gmail.com>
2025-10-31 17:04:11 +0700

    Membuat composable FormDataDiri

--- app/src/main/java/com/example/praktikum42/TeksEdit.kt
+++ app/src/main/java/com/example/praktikum42/TeksEdit.kt
@@ -38,3 +38,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 
 @Composable
+fun FormDataDiri(modifier: Modifier = Modifier) {
+    var textNama by remember { mutableStateOf("") }
+    var textAlamat by remember { mutableStateOf("") }
+    var textJK by remember { mutableStateOf("") }
